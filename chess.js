58eaeda74c5a1ff62813cf5662189ad51b3dfe5e
nathank07 <nathank7256@gmail.com>
2023-10-28 18:29:31 -0500

    Finished Knight

--- chess.js
+++ chess.js
@@ -19,16 +19,20 @@ const grid = [...Array(8)].map(e => Array(8).fill(null));
 
 function Piece ( { name, isWhite, availableMoves, board } ) { 
     return {
+
         name: name,
         isWhite: isWhite,
         availableMoves: availableMoves,
         board: board,
+
         display: () => {
             return isWhite ? whitePieces[name] : blackPieces[name]
         },
+
         canCapture: (capturedPiece) => {
             return capturedPiece ? isWhite !== capturedPiece.isWhite : false
         },
+
         move: (fromX, fromY, toX, toY) => {
             const moves = availableMoves(fromX, fromY) || [] 
             if(moves.some(xy => xy[0] === toX && xy[1] === toY)) {
@@ -36,6 +40,7 @@ function Piece ( { name, isWhite, availableMoves, board } ) {
                 board[fromX][fromY] = null
             }
         }
+
     }
 }
 
@@ -44,6 +49,7 @@ function Pawn( { isWhite, board } ) {
         name: "pawn",
         isWhite: isWhite,
         board: board,
+
         availableMoves: (xPos, yPos) => {
             const direction = isWhite ? 1 : -1
             const starting = isWhite ? xPos === 1 : xPos === 6
@@ -51,58 +57,132 @@ function Pawn( { isWhite, board } ) {
             for(let i = -1; i <= 1; i++) {
                 const x = xPos + direction
                 const y = yPos + i
+
+                if(outOfBounds(x, y)) { continue }
+
                 if(i !== 0 && piece.canCapture(board[x][y])) {
                     moves.push([x, y])
-                } else if (i === 0 && !piece.canCapture(board[x][y])) {
-                    moves.push([x, y])
+                } 
+
+                else if (i === 0 && !piece.canCapture(board[x][y])) {
                     if(starting) { moves.push([xPos + (direction * 2), y]) }
+                    moves.push([x, y])
                 }
+
             }
             return moves
         }
     })
     return piece;
 }
+function King( { isWhite, board } ) {
+    const piece = Piece({
+        
+        name: "king",
+        isWhite: isWhite,
+        board: board,
 
+        inCheck: (xPos, yPos) => {
+            let check = false;
+            board.forEach(row => {
+                row.forEach(square => {
+                    if(square.availableMoves().some(xy => xy[0] === xPos && xy[1] === yPos)) {
+                        check = true;
+                    }
+                })
+            });
+            return check;
+        },
+
+        availableMoves: (xPos, yPos) => {
+            let moves = []
+            for(let i = -1; i <= 1; i++) {
+                const x = i + xPos
+                for(let j = -1; j <= 1; j++) {
+                    const y = i + yPos
+
+                    if(outOfBounds(x, y)) { continue }
+
+                    if(board[x][y] === null || piece.canCapture(board[x][y])) {
+                        moves.push([(x, y)])
+                    }
+                }
+            }
+            return moves
+        },
+
+    })
+    return piece;
+}
+function Knight( { isWhite, board } ) {
+    const piece = Piece({
+        name: "knight",
+        isWhite: isWhite,
+        board: board,
+
+        availableMoves: (xPos, yPos) => {
+            let moves = [];
+            const knightMoves = [
+                [2, 1],
+                [1, 2],
+                [-2, 1],
+                [-1, 2],
+                [2, -1],
+                [1, -2],
+                [-2, -1],
+                [-1, -2]
+            ]
+            knightMoves.forEach(move => {
+                const x = xPos + move[0]
+                const y = yPos + move[1]
+                if(!outOfBounds(x, y) && (board[x][y] === null || piece.canCapture(board[x][y]))) {
+                    moves.push([x, y])
+                }
+            });
+            
+            return moves
+        }
+    })
+    return piece;
+}
 function createPiece(piece, isWhite) {
     let createdPiece = {};
+
     switch(piece){
         case "pawn":
-            createdPiece = Pawn({ isWhite: isWhite, board: grid })
+            createdPiece = Pawn( { isWhite: isWhite, board: grid } )
+            break
+        case "king":
+            createdPiece = King( { isWhite: isWhite, board: grid } )
+            break
+        case "knight":
+            createdPiece = Knight( { isWhite: isWhite, board: grid } )
             break
         default:
             createdPiece = { availableMoves: () => [] }
+            break
     }
+
     const genericPiece = Piece( {
         name: piece, 
         isWhite: isWhite,
         availableMoves: createdPiece.availableMoves,
-        board: grid} )
+        board: grid
+    })
+
     return {
         ...genericPiece,
         ...createdPiece
     }
 }
 
-grid[1][1] = createPiece("pawn", true)
-console.log(grid[1][1].display())
-//grid[1][1].move(1, 1, 3, 1)
-console.log(grid[1][1].availableMoves(1, 1))
-
-
-class King extends Piece {
-    constructor(isWhite, board) {
-        super("king", isWhite, board);
-    }
+function outOfBounds(x, y){
+    return x < 0 || y < 0 || x >= 8 || y >= 8
 }
+
 class Queen extends Piece {
     constructor(isWhite, board) {
         super("queen", isWhite, board);
-    }
-}
-class Knight extends Piece {
-    constructor(isWhite, board) {
-        super("knight", isWhite, board);
     }
 }
 class Bishop extends Piece {
@@ -117,51 +197,53 @@ class Rook extends Piece {
 }
 
 
-// for(let i = 0; i < 8; i++) {
-//     grid[1][i] = new Pawn(true, grid)
-//     grid[6][i] = new Pawn(false, grid)
-// }
+for(let i = 0; i < 8; i++) {
+    grid[1][i] = createPiece("knight", true)
+    grid[6][i] = createPiece("knight", false)
+}
 
-// function renderBoard() {
-//     const boardDiv = document.querySelector('.board') 
-//     boardDiv.innerHTML = ""
-//     let num = 0
-//     grid.forEach(row => {
-//         r = document.createElement('div');
-//         numDiv = document.createElement('span')
-//         numDiv.innerHTML = num
-//         r.appendChild(numDiv)
-//         row.forEach(square => {
-//             s = document.createElement('span');
-//             s.innerHTML = square ? square.display() : "#"
-//             r.appendChild(s);
-//         });
-//         num += 1
-//         boardDiv.appendChild(r)
-//     });
-//     r = document.createElement('div');
-//     for(let i = 0; i < 8; i++) {
-//         s = document.createElement('span');
-//         s.innerHTML = ` ${i}`
-//         r.appendChild(s)
-//     }
-//     boardDiv.appendChild(r)
-// }
-// renderBoard()
-// processInputs()
-// function processInputs() {
-//     const input = document.querySelector('input')
-//     input.addEventListener('keypress', (e) => {
-//         if(e.key === "Enter")
-//         {
-//             values = input.value.split(" ")
-//             values = values.map(x => Number(x))
-//             console.log(values)
-//             grid[values[0]][values[1]].move(values[0], values[1], values[2], values[3])
-//             renderBoard()
-//         }
-//     })
-// }
+function renderBoard() {
+    const boardDiv = document.querySelector('.board') 
+    boardDiv.innerHTML = ""
+    let num = 0
+    grid.forEach(row => {
+        r = document.createElement('div');
+        numDiv = document.createElement('span')
+        numDiv.innerHTML = num
+        r.appendChild(numDiv)
+        row.forEach(square => {
+            s = document.createElement('span');
+            s.innerHTML = square ? square.display() : "#"
+            r.appendChild(s);
+        });
+        num += 1
+        boardDiv.appendChild(r)
+    });
+    r = document.createElement('div');
+    for(let i = 0; i < 8; i++) {
+        s = document.createElement('span');
+        s.innerHTML = ` ${i}`
+        r.appendChild(s)
+    }
+    boardDiv.appendChild(r)
+}
+renderBoard()
+processInputs()
+function processInputs() {
+    const input = document.querySelector('input')
+    input.addEventListener('keypress', (e) => {
+        if(e.key === "Enter")
+        {
+            values = input.value.split(" ")
+            values = values.map(x => Number(x))
+            console.log(values)
+            grid[values[0]][values[1]].move(values[0], values[1], values[2], values[3])
+            renderBoard()
+        }
+    })
+}
 
+console.log(grid[1][1].availableMoves(1, 1))
+grid[1][1].availableMoves(1, 1)
 
 console.log(grid)
