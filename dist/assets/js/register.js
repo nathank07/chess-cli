708782e73af603afdac164b82c96774440294897
nathank07 <nathank7256@gmail.com>
2024-03-19 04:20:45 -0500

    Register page styling + clientside validation

--- dist/assets/js/register.js
+++ dist/assets/js/register.js
@@ -16,7 +16,37 @@
   \******************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _register_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./register.css */ \"./src/register/register.css\");\n\nfunction validateForm() {\n  var password = document.querySelector(\"form #password\").value;\n  var confirm = document.querySelector(\"form #confirm\").value;\n  if (password != confirm) {\n    alert(\"Passwords do not match.\");\n  }\n  return password == confirm;\n}\ndocument.querySelector('form').onsubmit = validateForm;\n\n//# sourceURL=webpack://my-webpack-project/./src/register/main.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _header_header_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../header/header.css */ \"./src/header/header.css\");\n/* harmony import */ var _footer_footer_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../footer/footer.css */ \"./src/footer/footer.css\");\n/* harmony import */ var _landing_landing_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../landing/landing.css */ \"./src/landing/landing.css\");\n/* harmony import */ var _register_css__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./register.css */ \"./src/register/register.css\");\n\n\n\n\nfunction validateForm() {\n  var username = document.querySelector(\"form #username\").value;\n  var usernameRegex = /^(?!.*[ ]{2,})(?!^[ ]|[ ]$)([a-zA-Z0-9_ -]{2,16})$/;\n  if (!usernameRegex.test(username)) {\n    alert(\"Invalid username\\n\\nUsername Requirements:\\n2-16 Characters\\nAlphanumeric, Space, Underscore, or Hyphen\\nNo Consecutive Spaces\\nNo Leading or Trailing Spaces\\n\");\n    return false;\n  }\n  var password = document.querySelector(\"form #password\").value;\n  var confirm = document.querySelector(\"form #confirm\").value;\n  if (password != confirm) {\n    alert(\"Passwords do not match.\");\n    return false;\n  }\n  var passwordRegex = /^(?=.*[A-Z])(?=.*\\d)[\\s\\S]{8,}$/;\n  if (!passwordRegex.test(password)) {\n    alert(\"Invalid password\\n\\nPassword Requirements:\\n8 Characters\\nAt least 1 Letter\\nAt least 1 Number\\n\");\n    return false;\n  }\n  return true;\n}\ndocument.querySelector('main form').onsubmit = validateForm;\n\n//# sourceURL=webpack://my-webpack-project/./src/register/main.js?");
+
+/***/ }),
+
+/***/ "./src/footer/footer.css":
+/*!*******************************!*\
+  !*** ./src/footer/footer.css ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://my-webpack-project/./src/footer/footer.css?");
+
+/***/ }),
+
+/***/ "./src/header/header.css":
+/*!*******************************!*\
+  !*** ./src/header/header.css ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://my-webpack-project/./src/header/header.css?");
+
+/***/ }),
+
+/***/ "./src/landing/landing.css":
+/*!*********************************!*\
+  !*** ./src/landing/landing.css ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://my-webpack-project/./src/landing/landing.css?");
 
 /***/ }),
 
--- src/register/main.js
+++ src/register/main.js
@@ -1,12 +1,27 @@
+import "../header/header.css"
+import "../footer/footer.css"
+import "../landing/landing.css"
 import "./register.css"
 
 function validateForm() {
+    const username = document.querySelector("form #username").value
+    const usernameRegex = /^(?!.*[ ]{2,})(?!^[ ]|[ ]$)([a-zA-Z0-9_ -]{2,16})$/
+    if(!usernameRegex.test(username)){
+        alert("Invalid username\n\nUsername Requirements:\n2-16 Characters\nAlphanumeric, Space, Underscore, or Hyphen\nNo Consecutive Spaces\nNo Leading or Trailing Spaces\n")
+        return false
+    }
     const password = document.querySelector("form #password").value
     const confirm = document.querySelector("form #confirm").value
     if(password != confirm) {
         alert("Passwords do not match.")
+        return false
     }
-    return password == confirm
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)[\s\S]{8,}$/
+    if(!passwordRegex.test(password)){
+        alert("Invalid password\n\nPassword Requirements:\n8 Characters\nAt least 1 Letter\nAt least 1 Number\n")
+        return false
+    }
+    return true;
 }
 
-document.querySelector('form').onsubmit = validateForm
+document.querySelector('main form').onsubmit = validateForm
